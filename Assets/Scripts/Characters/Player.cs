4e2fc584f5ccb6bfb931809ed851c14411422715
ldb385 <ldb385@usask.ca>
2019-09-29 16:41:25 -0600

    Added a simple player script and temp scene

--- Assets/Scripts/Characters/Player.cs
+++ Assets/Scripts/Characters/Player.cs
@@ -1,18 +1,34 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // Set a spook meter
+    public float spookLevel;
+    public float speed;
+    
+    // create rigid body
+    private Rigidbody2D rb;
+    // Create the movement velocity
+    private Vector2 moveVel;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 moveInput = new Vector2( Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveVel = moveInput.normalized * speed;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition( rb.position + moveVel * Time.fixedDeltaTime );
     }
 }
