4088d6a7c1b66a6f3ead3d4f56eb87e218833717
ldb385 <ldb385@usask.ca>
2019-09-30 12:52:00 -0600

    Added a sample scene to demo, and added simple AI script for enemy

---
+++ Assets/Scripts/Characters/Enemies.cs
@@ -0,0 +1,47 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Enemies : MonoBehaviour
+{
+    public Transform target;
+    
+    public float speed;
+    public float rangeDist;
+    public float chaseDist;
+    
+    
+    void Chase()
+    {
+        // this will be used to chase the player
+        transform.position = Vector2.MoveTowards(transform.position, target.position, 
+            speed * Time.deltaTime);
+        // may also apply damage?
+    }
+    
+    void Ranged()
+    {
+        // nothing yet
+    }
+    
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Vector2.Distance(transform.position, target.position) >= chaseDist)
+        {
+            Chase();
+        }
+        if (Vector2.Distance(transform.position, target.position) <= rangeDist)
+        {
+            Ranged();
+        }
+    }
+}
