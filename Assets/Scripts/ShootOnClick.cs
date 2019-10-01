4203bfbbc9448566589e021329bc815dcaccc9d3
tomasjohnsgaard <tomasjohnsgaard@icloud.com>
2019-09-30 21:53:15 -0600

    Made projectiles spawn on click in BattleTest

---
+++ Assets/Scripts/ShootOnClick.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootOnClick : MonoBehaviour
+{
+	public GameObject PlayerProjectile;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+		if (Input.GetMouseButtonDown(0))
+		{
+			Shoot();
+		}
+	}
+
+	void Shoot()
+	{
+		Vector3 clickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		clickPosition.z = 0;
+		Instantiate(PlayerProjectile, clickPosition, Quaternion.identity);
+	}
+}
