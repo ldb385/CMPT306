7b776813eab174ebf25f6ce4b499e7a340ab4f12
preston72ly <55118411+preston72ly@users.noreply.github.com>
2019-10-07 14:34:22 -0600

    Add files via upload
    added a power up script that makes the character's speed 1.5* as fast. (Soda Can)

---
+++ Assets/Scripts/Characters/PowerUp.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    public float multiplier = 1.5f;
+    public float duration = 10f;
+    public GameObject pickupEffect;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StartCoroutine(Pickup(other));
+        }
+        else
+        {
+            Debug.Log("Power up can't be picked up by enemies.");
+        }
+    }
+
+    IEnumerator Pickup(Collider player)
+    {
+        
+        //Spawn some kind of effect
+        Instantiate(pickupEffect, transform.position, transform.rotation);
+
+        //Apply effect to player
+        Player wiz = player.GetComponent<Player>();
+        wiz.speed *= multiplier;
+
+        //Timer
+        //Makes the game object disappear
+        GetComponent<SpriteRenderer>().enabled = false;
+        GetComponent<Collider>().enabled = false;
+        yield return new WaitForSeconds(duration);
+        //Returns to original value
+        wiz.speed /= multiplier;
+
+        //Clean Up and remove from scene
+        Destroy(gameObject);
+
+    }
+}
