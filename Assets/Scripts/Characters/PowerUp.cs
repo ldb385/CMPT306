60c88ccf393aa21cdf1f78428a1c5135068c4053
preston72ly <55118411+preston72ly@users.noreply.github.com>
2019-10-09 18:51:01 -0600

    Add files via upload
    added power up script

--- Assets/Scripts/Characters/PowerUp.cs
+++ Assets/Scripts/Characters/PowerUp.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class PowerUp : MonoBehaviour
 {
-    public float multiplier = 1.5f;
+    public float multiplier = 0.0000000000001f;
     public float duration = 10f;
     public GameObject pickupEffect;
 
-    void OnTriggerEnter(Collider other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
@@ -20,7 +20,7 @@ public class PowerUp : MonoBehaviour
         }
     }
 
-    IEnumerator Pickup(Collider player)
+    IEnumerator Pickup(Collider2D player)
     {
         
         //Spawn some kind of effect
@@ -28,15 +28,15 @@ public class PowerUp : MonoBehaviour
 
         //Apply effect to player
         Player wiz = player.GetComponent<Player>();
-        wiz.speed *= multiplier;
+        wiz.speed += multiplier;
 
         //Timer
         //Makes the game object disappear
         GetComponent<SpriteRenderer>().enabled = false;
-        GetComponent<Collider>().enabled = false;
+        GetComponent<Collider2D>().enabled = false;
         yield return new WaitForSeconds(duration);
         //Returns to original value
-        wiz.speed /= multiplier;
+        wiz.speed -= multiplier;
 
         //Clean Up and remove from scene
         Destroy(gameObject);
