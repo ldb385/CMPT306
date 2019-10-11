553b3b97047825a0d5115d5e46ede644f4ae18be
ldb385 <ldb385@usask.ca>
2019-10-11 13:45:22 -0600

    Added Skeleton and Ghost place holders with movement scenes attached in seperate folder

---
+++ Assets/Scripts/Characters/Ghost.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghost : MonoBehaviour
+{
+    // Ghost speed, Note, this should be faster then average since the player will be able
+    // to dodge attacks
+    public float speed;
+    
+    
+    /**
+     * More complex following command that uses ray-casting to find enemy then follows based off
+     * returned position, this allows for player to "sidestep" ghost in order to dodge attacks
+     */
+    void Chase()
+    {
+        // first need to send out a ray
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Characters/Skeleton.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Skeleton : MonoBehaviour
+{
+    
+    // this will be the player object
+    // Will be found based off tag in the start() function
+    private GameObject target;
+    
+    // following, used specifically for lookAt function
+    private Vector2 relativeTarget;
+    private bool lookingRight;
+    
+    // Ranged attack radius for skeleton attack
+    public float attackRange;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // set target as player
+        target = GameObject.FindWithTag( "Player" );
+        
+        // set orientation of enemy
+        lookingRight = true;
+        lookAt();
+    }
+    
+    
+    /**
+     * this function will just make the skeleton flip based off the player location
+     * basically just flips sprite based off player location
+     */
+    void lookAt()
+    {
+        // Check where player is located and turn towards
+        relativeTarget = transform.InverseTransformPoint( target.GetComponent<Transform>().position );
+        if( relativeTarget.x > 0f )
+        {
+            // On right side
+            if ( lookingRight ) // if not looking right, look other way
+            {
+                transform.Rotate(0f, 180f, 0f);
+                lookingRight = true;
+            }
+        }
+        else if( relativeTarget.x < 0f )
+        {
+            // On left side
+            if ( !lookingRight ) // if looking right, look left
+            {
+                transform.Rotate(0f, 180f, 0f);
+                lookingRight = false;
+            }
+        }
+    }
+    
+    
+    /**
+     * This will check if the enemy is in range and do an ranged attack if so
+     */
+    void inRange()
+    {
+        if ( Vector2.Distance(transform.position, target.GetComponent<Transform>().position) <= attackRange )
+        {
+            rangedAttack();
+        }
+    }
+    
+    /**
+     * Perform the ranged attack
+     */
+    void rangedAttack()
+    {
+        
+    }
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        // Can just run look at since checks are called in function
+        lookAt();
+        // check if in range and attack if so
+        inRange();
+        
+    }
+}
