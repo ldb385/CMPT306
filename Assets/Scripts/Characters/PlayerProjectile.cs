1537cb98fad8ed1b7d80616d736fa58df4c07d1e
tomasjohnsgaard <tomasjohnsgaard@icloud.com>
2019-10-20 17:48:08 -0600

    Moved projectile destruction to projectile prefab script
    Colliders not being detected properly as of now

---
+++ Assets/Scripts/Characters/PlayerProjectile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProjectile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // destroy projectile after 4 seconds if it hasn't hit anything
+        Destroy(gameObject, 4.0f);
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Obstacle") || collision.gameObject.CompareTag("Enemies"))
+        {
+            // play animation/sound?
+
+            Destroy(gameObject);
+        }
+    }
+}
