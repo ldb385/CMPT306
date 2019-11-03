a7bf40a17337c7583113ef3804404c6e4a30bd15
lmerilees <52306877+lmerilees@users.noreply.github.com>
2019-11-03 15:09:36 -0600

    Fixed audio issues for ghost

--- Assets/Scripts/Characters/Ghost.cs
+++ Assets/Scripts/Characters/Ghost.cs
@@ -1,15 +1,18 @@
-﻿
-using System;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 using Random = UnityEngine.Random;
 using Vector2 = UnityEngine.Vector2;
 
+
+
 public class Ghost : MonoBehaviour
+
 {
     // Ghost speed, Note, this should be faster then average since the player will not
     // immidiately be targeted
-    public float speed;
 
+    public float speed;
     public float health = 10f;
 
     // Stop enemy from ending up on top of player
@@ -26,24 +29,32 @@ public class Ghost : MonoBehaviour
     public AudioClip deathClip;
     public AudioClip chaseClip;
 
+    // bool for coroutine in ranged attack
+    public bool canLaugh = true;
+    public float cooldownTime;
+
     private Transform target;
-    
-    void calcuateNewMovementVector(){
+
+
+
+    void calcuateNewMovementVector()
+    {
         //create a random direction vector with the magnitude of 1, later multiply it with the velocity of the enemy
-        movementDirection = new Vector2(Random.Range( target.position.x - 0.3f, target.position.x + 0.3f), 
-            Random.Range( target.position.y , target.position.y )).normalized;
-        
-        movementPerSecond = movementDirection * 1.2f ;
+        movementDirection = new Vector2(Random.Range(target.position.x - 0.3f, target.position.x + 0.3f),
+            Random.Range(target.position.y, target.position.y)).normalized;
+        movementPerSecond = movementDirection * 1.2f;
     }
+
+
 
     private bool canSeePlayer()
     {
         // check if the enemy is close enough to charge
-        if (Vector2.Distance(transform.position, target.position) <=  visibilityDistance )
+        if (Vector2.Distance(transform.position, target.position) <= visibilityDistance)
         {
             return true;
         }
-        
+
         // the player was not detected within either view
         return false;
     }
@@ -51,23 +62,33 @@ public class Ghost : MonoBehaviour
     /**
      * run at players position
      */
+
     private void Chase()
     {
-
-        // rangedAttack(
-        if (period > laughInterval)
-        {
-            // play chase sounds
-            AudioSource.PlayClipAtPoint(chaseClip, transform.position);
-
-            period = 0;
-        }
-        period += UnityEngine.Time.deltaTime;
-
-
         // this will be used to chase the player
-        transform.position = Vector2.MoveTowards(transform.position, target.position, 
+        transform.position = Vector2.MoveTowards(transform.position, target.position,
             speed * Time.deltaTime);
+
+        // play laughter sound if not on cooldown
+        if (canLaugh)
+        {
+            StartCoroutine(Laugh());
+        }
+    }
+
+    public IEnumerator Laugh()
+    {
+        // play laugh
+        AudioSource.PlayClipAtPoint(chaseClip, transform.position);
+
+        // cooldown begins here
+        canLaugh = false;
+
+        // wait for cooldownTime seconds
+        yield return new WaitForSeconds(cooldownTime);
+
+        // cooldown time ends
+        canLaugh = true;
     }
 
     /**
@@ -76,30 +97,28 @@ public class Ghost : MonoBehaviour
     private void wander()
     {
         //if the changeTime was reached, calculate a new movement vector
-        if (Time.time - latestDirectionChangeTime > directionChangeTime){
+        if (Time.time - latestDirectionChangeTime > directionChangeTime)
+        {
             latestDirectionChangeTime = Time.time;
             calcuateNewMovementVector();
         }
-     
+
         //move enemy: 
-        transform.position = new Vector2(transform.position.x + (movementPerSecond.x * Time.deltaTime), 
+        transform.position = new Vector2(transform.position.x + (movementPerSecond.x * Time.deltaTime),
             transform.position.y + (movementPerSecond.y * Time.deltaTime));
     }
-
-
-    public float laughInterval;
-    private float period = 0.0f;
 
     /**
      * Executes enemy movement
      */
+
     private void movement()
     {
         if (Vector2.Distance(transform.position, target.position) > stopDist)
         {
             if (canSeePlayer())
-            {  
-             Chase();
+            {
+                Chase();
             }
             else
             {
@@ -108,12 +127,14 @@ public class Ghost : MonoBehaviour
         }
     }
 
+
+
     // Start is called before the first frame update
     void Start()
     {
         // Set player as target
         target = GameObject.FindGameObjectWithTag("Player").transform;
-        
+
         // Used for Enemy movement
         latestDirectionChangeTime = 0f;
         calcuateNewMovementVector();
@@ -125,7 +146,7 @@ public class Ghost : MonoBehaviour
         movement();
     }
 
-    private void OnCollisionEnter2D(Collision2D collision )
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "wall")
         {
@@ -136,13 +157,11 @@ public class Ghost : MonoBehaviour
         if (collision.gameObject.CompareTag("Projectile"))
         {
             health--;
-
             if (health <= 0)
             {
                 // play death sound and destroy object
                 //AudioSource.PlayClipAtPoint(deathClip, transform.position);
                 Destroy(gameObject);
-
             }
         }
     }
