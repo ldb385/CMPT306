bd01d4f1443e4f29fa0d0244dfea3d8bc1445736
ldb385 <ldb385@usask.ca>
2019-11-20 13:03:44 -0600

    Static class to hold points created, modifier of the class also created will need to attach to prefabs still

---
+++ Assets/Scripts/UI/PointModifier.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointModifier : MonoBehaviour
+{
+
+    // Two variables allow for easier abstraction to editor on modifiers
+    [SerializeField] private int pointAddition = 0;
+    [SerializeField] private int pointSubtraction = 0;
+    
+    // when object is destroyed update the point system
+    void OnDestroy()
+    {
+        if (Points.score + pointAddition - pointSubtraction <= 0)
+        {
+            Points.score = 0;
+        }
+        else
+        {
+            Points.score += pointAddition - pointSubtraction;   
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Points.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// this class will allow for comunication of score between scenes
+public static class Points
+{
+    public static int score
+    {
+        get 
+        {
+            return score;
+        }
+        set 
+        {
+            Debug.Log("PointUpdatesd");
+            score = value;
+        }
+    }
+}
