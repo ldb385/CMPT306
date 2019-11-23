b76bf2f2a15a90feb0ed85d2b423fa827e88c631
preston72ly <55118411+preston72ly@users.noreply.github.com>
2019-11-22 20:06:45 -0600

    Add files via upload
    added vaseDrop script. Drops a candy drop after the vase is destroyed

---
+++ Assets/Scripts/Characters/PlayerScripts/vaseDrop.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class vaseDrop : MonoBehaviour
+{
+	[SerializeField] private GameObject largePoint;
+    [SerializeField] private GameObject mediumPoint;
+    [SerializeField] private GameObject smallPoint;
+	
+		private bool blowUp = false;
+		private int drop;
+	
+	// detect if hit by projectile
+    void OnCollisionEnter2D(Collision2D col)
+    {
+		blowUp = true;
+    }
+    
+    // Raandomly selects a canday
+	public void Candy()
+	{
+		drop =(int) Random.Range( 0, 2 );
+			if( drop == 0 )
+			{
+				Instantiate(smallPoint,transform.position, Quaternion.identity);
+			}
+			else if( drop == 1 )
+			{
+				Instantiate(mediumPoint,transform.position, Quaternion.identity);
+			}
+			else
+			{
+				Instantiate(largePoint,transform.position, Quaternion.identity);
+			}
+	}
+
+    void Update()
+    {
+		if(blowUp == true)
+		{
+			Destroy(gameObject);
+			// Spawn candy drop
+			Candy();
+		}
+    }
+}
