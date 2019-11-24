fc047b84e31bc53e958c0c0ac987e05389b62c82
ldb385 <ldb385@usask.ca>
2019-11-24 11:57:49 -0600

    Accidently uploaded wrong one before

--- Assets/Scripts/ProcedralGeneration/Spawner.cs
+++ Assets/Scripts/ProcedralGeneration/Spawner.cs
@@ -26,8 +26,19 @@ public class Spawner : MonoBehaviour
     [SerializeField] private GameObject table;
     [SerializeField] private GameObject chairSide;
     [SerializeField] private GameObject chairBack;
+    [SerializeField] private GameObject vase;
+    
+    // ALL CANDY for Points
+    [SerializeField] private GameObject candyS;
+    [SerializeField] private GameObject candyM;
+    [SerializeField] private GameObject candyL;
+    
+    // ALL CANDY PARTICLES
+    [SerializeField] private ParticleSystem candySParticle;
+    [SerializeField] private ParticleSystem candyMParticle;
+    [SerializeField] private ParticleSystem candyLParticle;
 
-	// ALL DESPOOKERS
+    // ALL DESPOOKERS
 	[SerializeField] private GameObject Flashlight;
 	[SerializeField] private GameObject Teddy;
 	[SerializeField] private GameObject Candy;
@@ -73,7 +84,7 @@ public class Spawner : MonoBehaviour
 
     private GameObject getObstacle()
     {
-        int rnd = Random.Range(0, 4);
+        int rnd = Random.Range(0, 5);
 
         switch (rnd)
         {
@@ -85,6 +96,8 @@ public class Spawner : MonoBehaviour
                 return chairSide;
             case 3:
                 return chairBack;
+            case 4:
+	            return vase;
         }
 
         // It will never hit this
@@ -145,9 +158,9 @@ public class Spawner : MonoBehaviour
 
     private GameObject getEnemy( int rnd )
     {
-        if (rnd > 5)
+        if (rnd > 4)
         {
-            rnd = 5;
+            rnd = 4;
         }
 
         switch (rnd)
@@ -176,6 +189,7 @@ public class Spawner : MonoBehaviour
 		    val = 5;
 	    }
 
+	    // Just default
 	    ParticleSystem em = Instantiate(SkeletonParticle, space, Quaternion.identity);
 
 	    switch (val)
@@ -201,6 +215,57 @@ public class Spawner : MonoBehaviour
 	    em.Play();
     }
     
+    private GameObject getCandy( int rnd )
+    {
+	    if (rnd > 2)
+	    {
+		    rnd = 2;
+	    }
+
+	    switch (rnd)
+	    {
+		    case 0:
+			    return candyS;
+		    case 1:
+			    return candyM;
+		    case 2:
+			    return candyL;
+	    }
+
+	    // It will never hit this
+	    return candyS;
+    }
+    
+    
+    private void candyParticles( int val, Vector2 space )
+    {
+	    if (val > 2)
+	    {
+		    val = 2;
+	    }
+
+	    // just default
+	    ParticleSystem em = Instantiate( candySParticle, space, Quaternion.identity);
+
+	    switch (val)
+	    {
+		    case 0:
+			    em = Instantiate( candySParticle, space, Quaternion.identity );
+			    break;
+		    case 1:
+			    em = Instantiate( candyMParticle, space, Quaternion.identity );
+			    break;
+		    case 2:
+			    em = Instantiate( candyLParticle, space, Quaternion.identity );
+			    break;
+	    }
+
+	    // Play the animation
+	    em.Play();
+    }
+
+    
+    
     
     /**
      * Despawns the level and then spawns everything based on the most recent data model
@@ -223,7 +288,11 @@ public class Spawner : MonoBehaviour
                     tile = Instantiate( getObstacle(), (Vector2)i + diff, Quaternion.identity) as GameObject;
                     break;
                 case 2:
-	                int whichEn = Random.Range(0, (int) (9.0f - spookLevel) );
+	                int whichEn = Random.Range(0, (int) ((9.0f - spookLevel) /1.5f ) );
+	                if (whichEn > 4)
+	                {
+		                whichEn = Random.Range(2, 5);
+	                }
                     // Spawn in Enemy
                     tile = Instantiate(getEnemy(whichEn ),
                         (Vector2) i + diff, Quaternion.identity) as GameObject;
@@ -258,8 +327,15 @@ public class Spawner : MonoBehaviour
 						tile = Instantiate(getPowerUp((int) (9.0f - spookLevel)),
 							(Vector2) i + diff, Quaternion.identity) as GameObject;
                     }
-
                     break;
+                case 5:
+	                int whichCandy = Random.Range(0, 3 );
+	                // Spawn in Enemy
+	                tile = Instantiate(getCandy( whichCandy ),
+		                (Vector2) i + diff, Quaternion.identity) as GameObject;
+	                // Spawn in Particle effect around enemy
+	                candyParticles( whichCandy, i + diff );
+	                break;
 				default:
                     tile = null;
                     break;
@@ -286,26 +362,26 @@ public class Spawner : MonoBehaviour
     public Dictionary<Vector2Int, int> GenerateModel( int w, int h )
     {
 
+	    // this is the max amount of gameObjects that can be spawned
         int enemies = (int) ( 9.0f - spookLevel );
-
         if (enemies < minEnemies)
         {
             enemies = minEnemies;
         }
-
-        // this is the 
-		int powers = (int) spookLevel/2;
-		int despooks = (int) spookLevel/2;
+        int powers = (int) spookLevel/3;
+		int despooks = (int) spookLevel/3;
 
 
         int avelen = w + h / 2;
-        int obstacles = ( avelen * 3) / Random.Range( avelen/2, avelen) ;
-        int distFromObs = 3;
+        int obstacles = avelen*2 ;
+        // this represents the distance between objects
+        int distFromObs = 2;
         int distFromEne = 3;
 		int distFromPow = 3;
 		int distFromDes = 3;
+		int distFromCan = 2;
 
-        Dictionary<Vector2Int, int> tmp_model = new Dictionary<Vector2Int, int>();
+		Dictionary<Vector2Int, int> tmp_model = new Dictionary<Vector2Int, int>();
 
         int spawned = Random.Range(1, 5);
         
@@ -315,14 +391,18 @@ public class Spawner : MonoBehaviour
             {	// iterate through height
                 Vector2Int coord = new Vector2Int(i, j);
                 
-                spawned = spawned - 3;
+                spawned = spawned - 2;
 
                 if (spawned <= 0)
                 {
                     // Pick whether its empty, obstacle, enemy
-                    int rnd = Random.Range(0, avelen );
-                    
-                    
+                    int rnd = Random.Range(0, 9 );
+                    if ( avelen < 5 )
+                    {
+	                    rnd = Random.Range(0, 5);
+                    }
+
+
                     // need to make sure you are not spawning in front of a potential door
                     if (i == w / 2 && (j == 0 || j == h - 1) ||
                         j == h / 2 && (i == 0 || i == w - 1))
@@ -333,14 +413,14 @@ public class Spawner : MonoBehaviour
                     {
 	                    switch (rnd)
 	                    {
-		                    // 0 Empty, 1 Obstacle, 2 Enemey, 3 Powerup, 4 Despooker
+		                    // 0 Empty, 1 Obstacle, 2 Enemey, 3 Powerup, 4 Despooker, 5 Candy
 		                    case 1:
 			                    obstacles--;
 			                    if (obstacles <= 0 || distFromObs < 2)
 			                    {
 				                    // if there is too many or too close
 				                    rnd = 0;
-				                    spawned = Random.Range(0, avelen);
+				                    spawned = Random.Range(0, avelen /2);
 				                    distFromObs++;
 			                    }
 			                    else
@@ -355,7 +435,7 @@ public class Spawner : MonoBehaviour
 			                    {
 				                    // if there is too many or too close
 				                    rnd = 0;
-				                    spawned = Random.Range(0, avelen);
+				                    spawned = Random.Range(0, avelen /2);
 				                    distFromEne++;
 			                    }
 			                    else
@@ -370,7 +450,7 @@ public class Spawner : MonoBehaviour
 			                    {
 				                    // if there is too many or too close
 				                    rnd = 0;
-				                    spawned = Random.Range(0, avelen);
+				                    spawned = Random.Range(0, avelen /2);
 				                    distFromPow++;
 			                    }
 			                    else
@@ -386,7 +466,7 @@ public class Spawner : MonoBehaviour
 			                    {
 				                    // if there is too many or too close
 				                    rnd = 0;
-				                    spawned = Random.Range(0, avelen);
+				                    spawned = Random.Range(0, avelen /2);
 				                    distFromDes++;
 			                    }
 			                    else
@@ -396,6 +476,22 @@ public class Spawner : MonoBehaviour
 
 			                    despooks--;
 
+			                    break;
+		                    case 5:
+			                    if ( distFromCan < 2)
+			                    {
+				                    // if there is too many or too close
+				                    rnd = 0;
+				                    spawned = Random.Range(0, avelen /2);
+				                    distFromCan++;
+			                    }
+			                    else
+			                    {
+				                    distFromCan = 0;
+			                    }
+			                    break;
+		                    default:
+			                    rnd = 0;
 			                    break;
 	                    }
                     }
