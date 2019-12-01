f85f114e75533620d7fe70936e49a07ba3021a20
ldb385 <ldb385@usask.ca>
2019-12-01 17:49:30 -0600

    re-added scene transition

---
+++ Assets/Scripts/Level/NextScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class NextScene : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D col){
+      if(col.gameObject.CompareTag("Player")){
+        GameObject.Find("Player").GetComponent<Player>().SavePlayer();
+        GameObject.Find("Player").GetComponent<PlayerAttack>().SaveBalloons();
+        SceneManager.LoadScene( SceneManager.GetActiveScene().buildIndex +1);
+      }
+  }
+}
