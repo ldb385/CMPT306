327876898f5e13b0875981aa04f220182981e952
Michael <michael4161@hotmail.com>
2019-12-01 15:30:06 -0600

    boss room

--- Assets/Scripts/Level/NextFloor_1.cs
+++ Assets/Scripts/Level/NextFloor_1.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class NextScene : MonoBehaviour
+public class NextFloor_1 : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D col){
-      if(col.gameObject.CompareTag("Player")){
+      if(col.gameObject.tag == "Player"){
         GameObject.Find("Player").GetComponent<Player>().SavePlayer();
         GameObject.Find("Player").GetComponent<PlayerAttack>().SaveBalloons();
-        SceneManager.LoadScene( SceneManager.GetActiveScene().buildIndex +1 );
+        SceneManager.LoadScene("LevelTwo");
       }
   }
 }
